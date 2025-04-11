85c0578d8caa49fd97c502799e5417b7d2839ca6
bo090299 <batyromurkulow923@gmail.com>
2025-04-11 11:24:08 +0500

    11.04.2025 update

--- Program/src/Main/Screens/Onumcilik/Main_onumcilik_class.java
+++ Program/src/Main/Screens/Onumcilik/Main_onumcilik_class.java
@@ -1,12 +1,14 @@
 package Main.Screens.Onumcilik;
 
+import Main.Text_Classes.Bottom_text;
 import Main.Util.Box_class;
 import Main.Util.Button_class;
 import Main.Util.Connector;
 import Main.Util.SizeConfig;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 public class Main_onumcilik_class extends VBox {
@@ -29,9 +31,53 @@ public class Main_onumcilik_class extends VBox {
 
         DatePicker ondurilen_sene = new DatePicker();
         ondurilen_sene.setPromptText("Ondurilen senesi");
-        ondurilen_sene.setMaxSize(SizeConfig.Width(0.2),SizeConfig.Height(0.03));
+        ondurilen_sene.setMinSize(SizeConfig.Width(0.2),SizeConfig.Height(0.03));
 
+        DatePicker mohleti = new DatePicker();
+        mohleti.setPromptText("Mohleti");
+        mohleti.setMaxSize(SizeConfig.Width(0.2),SizeConfig.Height(0.03));
+        mohleti.setMinSize(SizeConfig.Width(0.2),SizeConfig.Height(0.03));
 
+        TextField umumymu = new TextField();
+        VBox umumy_box = box_class.Vbox_method("Umumy mukdary",umumymu,"Umumy mukdary");
+
+        TextField nace_girdeji = new TextField();
+        VBox nace_box = box_class.Vbox_method("Nace girdejilik onum",nace_girdeji,"Nace girdejilik onum");
+
+        Button update = button_class.Sized_btns("üýtgetmek", 0.06,0.04);
+        Button delete = button_class.Sized_btns("öçürmek",0.06,0.04);
+        Button input = button_class.Sized_btns("girizmek", 0.06,0.04);
+        Button yza = button_class.Sized_btns("çykmak", 0.06,0.04);
+
+        VBox birinji_btns = new VBox(15);
+        birinji_btns.getChildren().addAll(update,delete);
+        birinji_btns.setAlignment(Pos.CENTER);
+
+        VBox ikinji_btns = new VBox(15);
+        ikinji_btns.getChildren().addAll(input,yza);
+        ikinji_btns.setAlignment(Pos.CENTER);
+
+        HBox main_btns = new HBox(15);
+        main_btns.getChildren().addAll(birinji_btns,ikinji_btns);
+        main_btns.setAlignment(Pos.CENTER);
+
+        VBox esasy_box = new VBox(15);
+        esasy_box.getChildren().addAll(onum_box,kat_box,ady_box,bahasy_box,ondurilen_sene,mohleti,umumy_box,nace_box);
+        esasy_box.setAlignment(Pos.CENTER);
+
+        ScrollPane scroll = new ScrollPane();
+        scroll.setContent(new Onumcilik_list_class());
+
+        HBox main_box = new HBox(15);
+        main_box.getChildren().addAll(esasy_box,scroll);
+        main_box.setAlignment(Pos.CENTER);
+        scroll.setFitToWidth(true);
+        scroll.setFitToHeight(true);
+        main_box.setHgrow(esasy_box, Priority.ALWAYS);
+        main_box.setHgrow(scroll, Priority.ALWAYS);
+        this.getChildren().addAll(main_box);
+        esasy_box.setMaxSize(SizeConfig.Width(0.3),SizeConfig.Height(0.8));
+        esasy_box.setMinSize(SizeConfig.Width(0.3),SizeConfig.Height(0.8));
 
 
 
--- Program/src/Main/Screens/Onumcilik/Onumcilik_list_class.java
+++ Program/src/Main/Screens/Onumcilik/Onumcilik_list_class.java
@@ -1,6 +1,175 @@
 package Main.Screens.Onumcilik;
 
-public class Onumcilik_list_class {
-    public Onumcilik_list_class() {
+
+import Main.Util.Connector;
+import Main.Util.Label_class;
+import Main.Util.SizeConfig;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Onumcilik_list_class extends VBox {
+    public Onumcilik_list_class() throws SQLException {
+        Label_class label_class = new Label_class();
+        Connector connector = new Connector();
+        ScrollPane scroll = new ScrollPane();
+        String buyruk = "SELECT * FROM `onumcilik`";
+        Statement st = connector.getConnection().createStatement();
+        ResultSet rt = st.executeQuery(buyruk);
+
+        VBox dikine = new VBox();
+        int t_b = 0;
+        while (rt.next()){
+            t_b +=1;
+            Label tb = label_class.label(String.valueOf(t_b),0.06,0.04);
+
+            Label id = label_class.label(String.valueOf(rt.getInt("id")),0.06,0.04);
+
+            Label onum_id = label_class.label(String.valueOf(rt.getInt("onum_id")),0.06,0.04);
+
+            Label cat_id = label_class.label(String.valueOf(rt.getInt("kategoriya_id")), 0.06,0.04);
+
+            Label name = label_class.label(rt.getString("name"),0.08,0.04);
+
+            Label baha = label_class.label(String.valueOf(rt.getDouble("bahasy")),0.06,0.04);
+
+            Label ondurilen_sene = label_class.label(String.valueOf(rt.getDate("ondurilensenesi")),0.08,0.04);
+
+            Label mohleti = label_class.label(String.valueOf(rt.getDate("mohleti")),0.08,0.04);
+
+            Label umu_san = label_class.label(String.valueOf(rt.getInt("umumymukdary")),0.06,0.04);
+
+            Label nace_gir = label_class.label(String.valueOf(rt.getDouble("nace_girdejilik_onum")),0.06,0.04);
+
+            HBox hBox = new HBox();
+            hBox.getChildren().addAll(tb,id,onum_id,cat_id,name,baha,ondurilen_sene,mohleti,umu_san,nace_gir);
+
+            dikine.getChildren().addAll(hBox);
+
+        }
+
+        dikine.setAlignment(Pos.CENTER);
+        scroll.setContent(dikine);
+        scroll.hbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+        scroll.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+
+        //yokarky bolegi
+        HBox main_hbox = new HBox();
+
+        Label number = label_class.label("№",0.08,0.04);
+
+
+        Label id = label_class.label("Id",0.08,0.04);
+
+        Label cat_id = label_class.label("Kategoriýa id",0.08,0.04);
+
+        Label name = label_class.label("Ady",0.1,0.04);
+
+        Label bahasy = label_class.label("Bahasy",0.1,0.04);
+        Label info = label_class.label("Umumy maglumat",0.2,0.04);
+
+
+        main_hbox.getChildren().addAll(number,id,cat_id,name,bahasy,info);
+        main_hbox.setPrefWidth(SizeConfig.Width(0.6));
+
+        VBox all_vbox = new VBox();
+
+
+        HBox for_text = new HBox();
+        TextField textField =new TextField();
+        textField.setPromptText("Gözlemek üçin");
+        Label search = new Label("Gözleg...");
+        search.getStyleClass().add("Search");
+        textField.setPrefSize(SizeConfig.Width(0.2),SizeConfig.Height(0.04));
+        textField.getStyleClass().add("Gozleg");
+        VBox vBox =new VBox();
+        ObservableList<Onumler> onumler = FXCollections.observableArrayList();
+
+        textField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                try {
+
+                    String buyruk = "SELECT * FROM onumler WHERE name LIKE '%"+newValue+"%'";
+                    Statement st=connector.getConnection().createStatement();
+                    ResultSet rs=st.executeQuery(buyruk);
+                    int teb = 0;
+                    while (rs.next()){
+                        onumler.clear();
+                        onumler.addAll(new Onumler(rs.getInt("id"),rs.getInt("category_id"),rs.getString("name"),
+                                rs.getDouble("bahasy"),rs.getString("info")));
+
+                    }
+                    if(!textField.getText().isEmpty()){
+                        for (Onumler out : onumler){
+                            teb+=1;
+                            int finalTeb = teb;
+                            Platform.runLater(()->{
+                                int id = out.getId();
+                                int cd = out.getCategory_id();
+                                double baha = out.getBahasy();
+
+                                HBox jemi = new HBox();
+                                Label tb = label_class.label(String.valueOf(finalTeb),0.08,0.04);
+
+
+                                Label id1 = label_class.label(String.valueOf(id),0.08,0.04);
+
+                                Label cat_id = label_class.label(String.valueOf(cd),0.08,0.04);
+
+                                Label name = label_class.label(out.getName(),0.1,0.04);
+
+                                Label bahasy = label_class.label(String.valueOf(baha),0.1,0.04);
+
+
+                                Label info = label_class.label(out.getInfo(),0.2,0.04);
+
+
+                                jemi.getChildren().addAll(tb,id1,cat_id,name,bahasy,info);
+                                onumler.clear();
+                                vBox.getChildren().clear();
+                                vBox.getChildren().addAll(new VBox(jemi) );
+                                vBox.getChildren().add(0,all_vbox);
+
+                            });
+
+                        }
+                    }
+                    else {
+                        onumler.clear();
+                        vBox.getChildren().clear();
+                        vBox.getChildren().add(scroll);
+                        vBox.getChildren().add(0,all_vbox);
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        for_text.getChildren().addAll(search,textField);
+
+        for_text.setAlignment(Pos.CENTER_LEFT);
+
+        all_vbox.getChildren().addAll(for_text,main_hbox);
+        vBox.getChildren().addAll(all_vbox);
+        vBox.getChildren().add(1,onumler.size() == 0 ? scroll : null);
+
+        this.getChildren().addAll(vBox);
+        this.setPrefSize(SizeConfig.Width(0.8),SizeConfig.Height(0.8));
     }
 }
